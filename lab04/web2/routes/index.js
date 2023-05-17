af6ebd6d071ef7e5587eddd3b2362bb2149b4e99
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-05-17 15:54:21 +0700

    cloud

--- lab04/web2/routes/index.js
+++ lab04/web2/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//1. render ra form input
+router.get('/input', (req, res) => {
+  res.render('input');
+})
+
+//2. lấy dữ liệu từ form input
+router.post('/output', (req, res) => {
+  //A. in ra console log
+  console.log(req.body);
+  //B. in ra web với res.send
+  res.send(req.body);
+})
 
 module.exports = router;
