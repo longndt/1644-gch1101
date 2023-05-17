ee1a1415e4c66ebcfa11c328b675faf50ce8ad8c
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-05-17 16:52:16 +0700

    cloud

--- lab04/web2/routes/index.js
+++ lab04/web2/routes/index.js
@@ -25,4 +25,19 @@ router.post('/output', (req, res) => {
   //res.render('output', { name : name, age : age, email: email, dob : dob});
 })
 
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+router.post('/check', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == 'admin' && password == '123456') {
+      res.send("<h1 style='color: blue; background-color: lime';>Login successfully !</h1>");
+    } else {
+      res.send("<h1 style='color: red; background-color: yellow';>Login failed !</h1>");
+    }
+})
+
 module.exports = router;
