319f3a99bdf629eec03e2194fbe167ecebc486ac
Long <longndt226@gmail.com>
2023-06-05 16:31:41 +0700

    1101

--- lab09/web/routes/movie.js
+++ lab09/web/routes/movie.js
@@ -24,33 +24,46 @@ router.post('/rent', async (req, res) => {
    res.render("movie/rent", { movie : movie })
 })
 
+router.get('/add', (req, res) => {
+   res.render('movie/add');
+})
 
+router.post('/add', async (req, res) => {
+   var movie = req.body;
+   await MovieModel.create(movie)
+   .then(() => { console.log ("Add new movie succeed !")});
+   res.redirect('/movie');
+})
 
+router.get('/edit/:id', async (req, res) => {
+   var id = req.params.id;
+   var movie = await MovieModel.findById(id);
+   res.render('movie/edit', { movie : movie });
+})
 
-
-
-
-
-
-
-
-
+router.post('/edit/:id', async (req, res) => {
+   var id = req.params.id;
+   await MovieModel.findByIdAndUpdate(id)
+      .then(() => { console.log("Edit movie succeed !") });
+   res.redirect('/movie');
+})
 
 //search function
 router.post('/search', async (req, res) => {
-   var movies = await MovieModel.find({ title: new RegExp(req.body.title, "i")})
-   res.render('movie/index', { movies: movies })
+   var keyword = req.body.title;
+   var movies = await MovieModel.find({ title: new RegExp(keyword, "i")})
+   res.render('movie/list', { movies: movies })
 })
 
 //sort function
 router.get('/sort/asc', async (req, res) => {
    var movies = await MovieModel.find().sort({ title: 1 })
-   res.render('movie/index', { movies: movies })
+   res.render('movie/list', { movies: movies })
 })
 
 router.get('/sort/desc', async (req, res) => {
    var movies = await MovieModel.find().sort({ title: -1 })
-   res.render('movie/index', { movies: movies })
+   res.render('movie/list', { movies: movies })
 })
 
 module.exports = router;
